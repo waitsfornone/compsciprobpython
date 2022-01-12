cf1e5fee285f478e76e40fa3e46fc691bb19bb38
Tim Enders <enders.tim@gmail.com>
2022-01-12 12:29:43 -0600

    starting the maze searches

--- dna_search.py
+++ dna_search.py
@@ -5,7 +5,6 @@ Nucleotide: IntEnum = IntEnum("Nucleotide", ("A", "C", "G", "T"))
 Codon = Tuple[Nucleotide, Nucleotide, Nucleotide] # type alias for Codons
 Gene = List[Codon] # type alias for Genes
 
-gene_str: str = "ACGTGGCGCGCGAACGTACGTACGTACGGGGTTTATATATACCCTAGGACTCCCTTT"
 
 def string_to_gene(s: str) -> Gene:
     gene: Gene = []
@@ -17,4 +16,35 @@ def string_to_gene(s: str) -> Gene:
         gene.append(codon) # add codon to gene
     return gene
 
-my_gene: Gene = string_to_gene(gene_str)
+
+def linear_contains(gene: Gene, key_codon: Codon) -> bool:
+    for codon in gene:
+        if codon == key_codon:
+            return True
+        
+    return False
+
+
+def binary_contains(gene: Gene, key_codon: Codon) -> bool:
+    low: int = 0
+    high: int = len(gene) - 1
+    while low <= high: # while there is still a search space
+        mid: int = (low + high) // 2
+        if gene[mid] < key_codon:
+            low = mid + 1
+        elif gene[mid] > key_codon:
+            high = mid - 1
+        else:
+            return True
+
+    return False
+
+
+if __name__ == "__main__":
+    gene_str: str = "ACGTGGCGCGCGAACGTACGTACGTACGGGGTTTATATATACCCTAGGACTCCCTTT"
+    my_gene: Gene = string_to_gene(gene_str)
+    acg: Codon = (Nucleotide.A, Nucleotide.C, Nucleotide.G)
+    gat: Codon = (Nucleotide.G, Nucleotide.A, Nucleotide.T)
+    my_sorted_gene: Gene = sorted(my_gene)
+    print(binary_contains(my_sorted_gene, acg))
+    print(binary_contains(my_sorted_gene, gat))
---
+++ maze.py
@@ -0,0 +1,71 @@
+from enum import Enum
+from typing import List, NamedTuple, Callable, Optional
+import random
+from math import sqrt
+# from generic_search import dfs, bfs, node_to_path, astar, Node
+
+
+class Cell(str, Enum):
+    EMPTY = " "
+    BLOCKED = "X"
+    START = "S"
+    GOAL = "G"
+    PATH = "*"
+
+
+class MazeLocation(NamedTuple):
+    row: int
+    column: int
+
+
+class Maze:
+    def __init__(self, rows: int = 10,
+                 columns: int = 10,
+                 sparseness: float = 0.2,
+                 start: MazeLocation = MazeLocation(0, 0),
+                 goal: MazeLocation = MazeLocation(9, 9)
+                ) -> None:
+        # initialize basic instance variables
+        self._rows: int = rows
+        self._columns: int = columns
+        self.start: MazeLocation = start
+        self.goal: MazeLocation = goal
+        # fill the grid with empty cells
+        self._grid: List[List[Cell]] = [[Cell.EMPTY for c in range(columns)] for r in range(rows)]
+        # populate the grid with blocked cells
+        self._randomly_fill(rows, columns, sparseness)
+        # fill the start and goal locations
+        self._grid[start.row][start.column] = Cell.START
+        self._grid[goal.row][goal.column] = Cell.GOAL
+
+    def _randomly_fill(self, rows: int, columns: int, sparseness: float):
+        for row in range(rows):
+            for column in range(columns):
+                if random.uniform(0, 1.0) < sparseness:
+                    self._grid[row][column] = Cell.BLOCKED
+
+    def __str__(self) -> str:
+        output: str = ""
+        for row in self._grid:
+            output += "".join([c.value for c in row]) + "\n"
+        return output
+
+    def goal_test(self, ml: MazeLocation) -> bool:
+        return ml == self.goal
+
+    def successors(self, ml: MazeLocation) -> List[MazeLocation]:
+        locations: List[MazeLocation] = []
+        if ml.row + 1 < self._rows and self._grid[ml.row + 1][ml.column] != Cell.BLOCKED:
+            locations.append(MazeLocation(ml.row + 1, ml.column))
+        if ml.row - 1 >= 0 and self._grid[ml.row - 1][ml.column] != Cell.BLOCKED:
+            locations.append(MazeLocation(ml.row - 1, ml.column))
+        if ml.column + 1 < self._columns and self._grid[ml.row][ml.column + 1] != Cell.BLOCKED:
+            locations.append(MazeLocation(ml.row, ml.column + 1))
+        if ml.column - 1 >= 0 and self._grid[ml.row][ml.column - 1] != Cell.BLOCKED:
+            locations.append(MazeLocation(ml.row, ml.column - 1))
+        return locations
+
+
+if __name__ == "__main__":
+    maze: Maze = Maze()
+    print(maze)
